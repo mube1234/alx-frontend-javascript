e2bdcc3838fde21ceade2f8a7961e5cc32317328
Mubarik <burkatech2021@gmail.com>
2023-06-29 17:34:38 +0000

    pricing_correction

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,36 +1,44 @@
 import Currency from './3-currency';
 
 export default class Pricing {
-  /* eslint-disable */
   constructor(amount, currency) {
-    this._currency = currency;
-    this._amount = amount;
-  }
-
-  // getter 
-  get currency() {
-    return this._currency;
+    this.amount = amount;
+    this.currency = currency;
   }
 
   get amount() {
     return this._amount;
   }
 
-  // setter
-  set currency(Currency) {
-    this._currency = Currency;
+  set amount(amount) {
+    if ((typeof amount !== 'number') && !(amount instanceof Number)) {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = amount;
   }
 
-  set amount(Amount) {
-    this._amount = Amount;
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instanceof of Currency');
+    }
+    this._currency = currency;
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.displayFullCurrency()}`; 
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
-  // static that calculates the amount multiplied by the conversion rate
   static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('Conversion rate must be a number');
+    }
     return amount * conversionRate;
   }
 }
